8bf9b1a461af3776fb610a488a80a77ccff9d421
jxmaymay <31222311+jxmaymay@users.noreply.github.com>
2017-11-28 07:10:28 -1000

    Add files via upload

---
+++ InternetInterfacer.java
@@ -0,0 +1,143 @@
+import java.io.*;
+import java.net.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+/**
+ * This class provides the scaffolding to send an http request to the
+ * Google Maps API, get a packet of XML data in return, and parse
+ * that data into a DOM document (a tree-like structure).
+ *
+ * @author Michael Ida
+ */
+public class InternetInterfacer {
+
+	public static void main(String[] args) throws IOException,
+	ParserConfigurationException, SAXException {
+
+		/*
+		 * Set up the parameters to send a http query
+		 */
+		String base = "https://maps.googleapis.com/maps/api/directions/xml?";
+		String origin = "origin=4680+Kalanianaole+Highway+Honolulu+96821";
+		String dest = "destination=Kahala+Mall+Honolulu";
+		String extras = "mode=walking";
+		String key = "key=AIzaSyAbPoFI7ul8T8h_khn3r1LPBBJYatDSPIQ";
+		String urlString = base + "&" + origin + "&" + dest + "&" +
+				extras + "&" + key;
+		URL url = new URL(urlString);
+		HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+		httpCon.setDoOutput(true);
+		httpCon.setRequestMethod("GET");
+
+		/*
+		 * Set up a buffered reader to receive the response
+		 */
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(httpCon.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+			System.out.println(inputLine);
+		}
+		in.close();
+
+		/*
+		 * Convert the response into a string and trim off leading
+		 * characters which would throw the parser off
+		 */
+		String responseMessage = response.toString();
+		responseMessage = responseMessage.trim().replaceFirst("^([\\W]+)<","<");
+
+		/*
+		 * Pass the XML string through the XML parser and convert it into
+		 * a DOM document (a tree-like structure)
+		 */
+		DocumentBuilder db =
+				DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		Document doc =
+				db.parse(new InputSource(new StringReader(responseMessage)));
+	
+		/*
+		 * This is where you begin.  Figure out how the Document data structure
+		 * works and how to find and extract the information you want from it.
+		 * Use that information to output something useful and interesting to
+		 * the end-user.  The document 'doc' contains all of the data from
+		 * the XML parser, and this is the object that you'll want to work
+		 * with.
+		 */
+		
+		Element root = doc.getDocumentElement();
+	
+		NodeList a = root.getChildNodes();
+		
+		Node b = a.item(3);
+		System.out.println("Start at: " + a.item(3).getTextContent());
+		System.out.println("Destination is:" + a.item(7).getTextContent());
+	}
+
+		/*	if (doc.hasChildNodes()) {
+
+				printNote(doc.getChildNodes());
+
+			}
+
+		    } catch (Exception e) {
+			System.out.println(e.getMessage());
+		    }
+
+		  }
+		*/
+	/*	  private static void printNote(NodeList nodeList) {
+
+		    for (int count = 0; count < nodeList.getLength(); count++) {
+
+			Node tempNode = nodeList.item(count);
+
+			// make sure it's element node.
+			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				// get node name and value
+				System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
+				System.out.println("Node Value =" + tempNode.getTextContent());
+
+				if (tempNode.hasAttributes()) {
+
+					// get attributes names and values
+					NamedNodeMap nodeMap = tempNode.getAttributes();
+
+					for (int i = 0; i < nodeMap.getLength(); i++) {
+
+						Node node = nodeMap.item(i);
+						System.out.println("attr name : " + node.getNodeName());
+						System.out.println("attr value : " + node.getNodeValue());
+
+					}
+
+				}
+
+				if (tempNode.hasChildNodes()) {
+
+					// loop again if has child nodes
+					System.out.print(tempNode.getChildNodes());
+
+				}
+
+				System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
+
+			}
+
+		    }
+
+		  }
+*/
+	  /*  doc.getDocumentElement().normalize();
+		System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+		System.out.println("Start: " + doc.getDocumentElement().isDefaultNamespace(origin));
+		System.out.println("Start: " + doc.getDocumentElement().getElementsByTagName(origin));
+		System.out.println("Start: " + doc.getDocumentElement().getBaseURI());
+		System.out.println(doc);
+		*/
+}
